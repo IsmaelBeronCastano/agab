d731723959c155116fd665a48e7f267836ab0a9e
Ismael Berón <portafolioismaelberon@gmail.com>
2024-04-08 18:14:08 +0200

    footer problems 1

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,9 @@
 
 const Footer = () => {
   return (
-    <footer className="w-full h-[100px] flex sm:flex-col md:flex-row md:w-full items-center md:justify-around mt-2 bg-purple-800">
-      <p className="text-black font-bold text-2xl lobster pl-2">Amics dels Gats i d'altres Animals de Balenyà</p>
-      <h1 className="text-white font-bold text-7xl alfa-slab pt-2">A.G.A.B.</h1>
+    <footer className="w-full h-[100px] flex sm:flex-col flex-wrap md:flex-row md:w-full items-center md:justify-around mt-2 bg-purple-800">
+      <p className="text-black font-bold text-xl lobster pl-2">Amics dels Gats i d'altres Animals de Balenyà</p>
+      <h1 className="text-white font-bold text-4xl alfa-slab pt-2">A.G.A.B.</h1>
     </footer>
   );
 }
