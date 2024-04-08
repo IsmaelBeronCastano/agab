9ce493cc90401caae3c4dc71a28fd541ebaba9e6
Ismael Berón <portafolioismaelberon@gmail.com>
2024-04-08 18:43:04 +0200

    ajustos

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,8 +6,10 @@ const Header = () => {
         <div className="flex items-center">
             <img src={Image} id="agab" alt="Agab" className="w-20 h-full" />
         </div>
-        <p className="text-black font-bold text-2xl lobster pl-2 pb-3">Amics dels Gats i d'altres Animals de Balenyà</p>
-        <h1 className="text-white font-bold text-7xl alfa-slab pt-2">A.G.A.B.</h1>
+        <div className="flex flex-col align-center">
+          <h1 className="text-white font-bold text-7xl alfa-slab pt-2 pl-16">A.G.A.B.</h1>
+          <p className="text-black font-bold text-2xl lobster pt-2 pb-3">Amics dels Gats i d'altres Animals de Balenyà</p>
+        </div>
         <div className="flex flex-col  flex-start items-center pr-8">
             <a href="#" className="text-lg text-black rounded-md p-1 border-4 border-black lora mb-3 bg-white">bizzum: 689969006</a>
             <a href="#" className="text-lg text-black rounded-md p-1 border-4 border-black lora mb-3  bg-white">agabanimals@gmail.com</a>  
